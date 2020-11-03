a637013a80448486472a66254c962db0fe11d574
fu1819 <71689568+fu1819@users.noreply.github.com>
2020-11-03 15:56:17 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+basic.forever(function () {
+    for (let I = 0; I <= 4; I++) {
+        for (let j = 0; j <= I; j++) {
+            led.plot(j, 4 - I)
+            led.plot(I, 4 - j)
+        }
+        basic.pause(500)
+    }
+    for (let I = 0; I <= 4; I++) {
+        for (let j = 0; j <= I; j++) {
+            led.unplot(j, 4 - I)
+            led.unplot(I, 4 - j)
+        }
+        basic.pause(500)
+    }
+})
